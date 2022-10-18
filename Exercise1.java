5471a00d9054697d495fabfe81be61227ca12f77
VegadGhanshyam <ghanshyamv@prominentpixel.com>
2022-10-18 11:29:18 +0530

    first javaExercise1.java

---
+++ Exercise1.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.*;
+
+public class Exercise1 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter First name");
+        String fname=sc.next();
+        System.out.println("Enter Last name");
+        String lname=sc.next();
+        System.out.println("Enter middle name");
+        String mname=sc.next();
+        System.out.println(fname+" "+mname+" " +lname);
+
+    }
+
+
+}
